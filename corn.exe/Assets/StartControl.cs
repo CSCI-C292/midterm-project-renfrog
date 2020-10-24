b69baa4ad22d6ac51ef97e7c1fb4f70b00afbed7
Gracie Renfro <renfrog@iu.edu>
2020-10-24 14:15:37 -0400

    buttons and menu

---
+++ corn.exe/Assets/StartControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // found guidance on this at this link https://forum.unity.com/threads/using-a-button-to-switch-scenes.379945/
+    public void NextScene(string name) {
+        SceneManager.LoadScene(name);
+    }
+}
